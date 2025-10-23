941c8041e5e5f25cf7216b7837edd4d44bba1d89
Germanius <german.lippert@mail.ru>
2025-10-22 19:37:15 -0700

    Installed Leaflet for map work + began work on MapSection

---
+++ myGreenApp/src/HomeScreen/MapSection.jsx
@@ -0,0 +1,11 @@
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function MapSection() {
+    return (
+        <div className="map-container">
+            
+        </div>
+    )
+}
